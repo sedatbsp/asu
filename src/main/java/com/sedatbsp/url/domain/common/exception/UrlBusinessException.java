1c3b04f8dc6e8cdfc361e1da98762ba331876a2c
Sedat Başpınar <sdtbsp10@gmail.com>
2022-07-15 18:02:44 +0300

    url business exception added

---
+++ src/main/java/com/sedatbsp/url/domain/common/exception/UrlBusinessException.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.url.domain.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UrlBusinessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String key;
+}
--- src/main/java/com/sedatbsp/url/domain/common/exception/UrlException.java
+++
@@ -1,4 +0,0 @@
-package com.sedatbsp.url.domain.common.exception;
-
-public class UrlException {
-}
