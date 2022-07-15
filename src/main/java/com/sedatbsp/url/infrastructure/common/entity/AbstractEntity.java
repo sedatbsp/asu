c19402e4eedd9796cad80c06d96a514dc68745e9
Sedat Başpınar <sdtbsp10@gmail.com>
2022-07-15 17:51:32 +0300

    AbstractEntity added

---
+++ src/main/java/com/sedatbsp/url/infrastructure/common/entity/AbstractEntity.java
@@ -0,0 +1,33 @@
+package com.sedatbsp.url.infrastructure.common.entity;
+
+import com.sedatbsp.url.domain.common.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", length = 400)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", length = 400)
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+
+}
